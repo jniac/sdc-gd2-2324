ea0e76c9d12f4186101631ce61a584670ec0d88b
jniac <joseph.merdrignac@gmail.com>
2023-10-12 09:39:07 +0200

    checkfolder is ok

--- info/scripts/check-folder.mjs
+++ info/scripts/check-folder.mjs
@@ -1,16 +1,44 @@
-import yaml from 'js-yaml'
 import fs from 'fs/promises'
+import yaml from 'js-yaml'
+import chalk from 'chalk'
 
-const data = await fs.readFile('info/promotion.yaml', { encoding: 'utf-8' })
+/**
+ * @typedef {object} Student
+ * @property {string[]} names
+ * @property {string} github
+ * @property {string} prefix
+ */
+
+/** @type {{ promotion: Student[] }} */
+const data = await fs.readFile('../promotion.yaml', { encoding: 'utf-8' })
   .then(text => yaml.load(text))
 
-async function filesExists(folder) {
+/**
+ * 
+ * @param {string} folder 
+ * @param {(student: Student) => string} fileNamePredicate 
+ * @returns 
+ */
+async function filesExists(folder, fileNamePredicate = student => student.github) {
   try {
-    await Promise.all(data.promotion.map(student => fs.lstat(`art/${student.github}`)))
-  } catch (err) {
-    return `"${err.path}" is missing`
+    const files = (await fs.readdir(folder)).filter(file => file.startsWith('.') === false)
+    const studentFiles = data.promotion.map(student => fileNamePredicate(student))
+    const missingFiles = studentFiles.filter(studentFile => files.includes(studentFile) === false)
+    const extraFiles = files.filter(file => studentFiles.includes(file) === false)
+
+    const extraFilesInfo = chalk.dim(extraFiles.length > 0 ? `(extras files: ${extraFiles.join(', ')})` : '(no extra files)')
+    if (missingFiles.length > 0) {
+      return `${chalk.red('missing files!')} ${missingFiles.join(', ')} ${extraFilesInfo}`
+    } else {
+      return `${chalk.greenBright('ok')} ${extraFilesInfo}`
+    }
+  } catch (error) {
+    return `Invalid folder!` 
   }
-  return 'ok'
 }
 
-console.log(`web folders: ${await filesExists()}`)
+const webArtFolder = '../../art'
+console.log(`web folders: ${await filesExists(webArtFolder)}`)
+
+const unrealArtFolder = '/Users/joseph/Documents/Unreal Projects/SDC_GD2_2324_UE5/Content/Art'
+console.log(`unreal folders: ${await filesExists(unrealArtFolder, ({ prefix, github }) => `${prefix}_${github}`)}`)
