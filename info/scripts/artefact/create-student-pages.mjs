8ce4491bc82e4e6a718d182fcc1fc1e3271cd266
jniac <joseph.merdrignac@gmail.com>
2024-01-29 11:21:21 +0100

    better template

--- info/scripts/artefact/create-student-pages.mjs
+++ info/scripts/artefact/create-student-pages.mjs
@@ -1,10 +1,44 @@
 import fs from 'fs/promises'
+import path from 'path'
 import { data } from '../data.mjs'
+
+const capitalize = str => str[0].toUpperCase() + str.slice(1).toLowerCase()
+
+async function filesExists(filepath) {
+  try {
+    await fs.access(filepath)
+    return true
+  } catch (error) {
+    return false
+  }
+} 
 
 const students = data.promotion
   .filter(student => student.github !== 'IAmSoDIM') // already done
 
+const srcDir = `artefact/template/`
+const files = [...await fs.readdir(srcDir)]
+  .map(file => path.join(srcDir, file))
+
+const entries = await Promise.all(files.map(async file => {
+  const str = await fs.readFile(file, { encoding: 'utf-8' })
+  return { file, str }
+}))
+
 for (const student of students) {
-  const dst = `../../art/${student.github}/artefact`
-  fs.cp(`artefact`, dst, { recursive: true })
+  const dstDir = `../../art/${student.github}/artefact/`
+  for (const { file, str } of entries) {
+    await fs.mkdir(dstDir, { recursive: true })
+    const dst = path.join(dstDir, path.basename(file))
+    const modifiedStr = str
+      .replaceAll(/\$GITHUB/g, student.github)
+      .replaceAll(/\$NAMES/g, student.names.map(capitalize).join(' '))
+    
+    if (await filesExists(dst)) {
+      console.log(`skipping ${dst} (already exists)`)
+      continue      
+    }
+    
+    fs.writeFile(dst, modifiedStr)
+  }
 }
